5404025c07447f11067a88c4a16576f86e0c654b
Voronko Sergey <vss1502@i.ua>
2021-03-01 14:31:51 +0200

    init:start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array|| array.length==0) {
+    return 0;
+  }
+  
+  let m = array[0];
+  for (let i =1; i<array.length; i++) {
+    if(array[i]<m) {
+      m = array[i];
+    }
+  }
+  return m;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array|| array.length==0) {
+    return 0;
+  }
+  let ma = array[0];
+  for (let i =1; i<array.length; i++) {
+    if(array[i]>ma) {
+      ma = array[i];
+    }
+  }
+  return ma;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array|| array.length==0) {
+    return 0;
+  }
+ 
+  let sum = 0;
+  for (let i =0; i<array.length; i++) {
+    sum = sum +array[i];
+  }
+  av = sum/array.length;
+  
+  return av;
 }
